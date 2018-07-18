fc82de9a43aad43fc2b5e27e4a09d0f7979ba831
Vytautas Vaičys <vytautas@vaicys.com>
2018-07-18 21:40:55 +0200

    Initial source import of slimmed down ASP.NET Core template.

---
+++ WebApp/Controllers/MainController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using apps_vaicys_com_netcore.Models;
+
+namespace apps_vaicys_com_netcore.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            throw new Exception("Test");
+            return Redirect("http://www.vaicys.com");
+            //return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace apps_vaicys_com_netcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
